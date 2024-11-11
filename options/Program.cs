755a5d479d2ef84e12de16bbaaae54db2750dea7
4194304 <130800536+4194304@users.noreply.github.com>
2024-11-11 20:04:09 +0000

    Add files via upload

---
+++ options/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+static void RunTask(string file) {
+  ProcessStartInfo startInfo = new ProcessStartInfo() {
+    FileName = $"{file}",
+  };
+  Process proc = new Process() { StartInfo = startInfo, };
+  proc.Start();
+}
+Console.WriteLine();
+Console.WriteLine("1. Shutdown");
+Console.WriteLine("2. Reboot");
+Console.WriteLine("3. Logoff");
+Console.WriteLine("4. Hibernate");
+Console.WriteLine("5. Exit");
+Console.Write("Select the action would you like to perform: ");
+int input = Convert.ToInt32(Console.ReadLine());
+switch(input) {
+  case 1:
+    RunTask("shutdown -P 0");
+    break;
+  case 2:
+    RunTask("reboot");
+    break;
+  case 3:
+    RunTask("killall openbox");
+    break;
+  case 4:
+    RunTask("hibernate");
+    break;
+  case 5:
+    break;
+}
